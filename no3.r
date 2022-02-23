27935b9e4a0525e340eb08dfc594d078ad981601
SeokJeong0411 <97781970+SeokJeong0411@users.noreply.github.com>
2022-02-23 23:44:11 +0900

    Update no3.r

--- no3.r
+++ no3.r
@@ -3,58 +3,65 @@ library(tidyverse)
 library(shiny)
 library(hrbrthemes)
 library(gganimate)
+library(readxl)
 
 ui <- fluidPage(
     # 1. 헤드 패널
     headerPanel(h1('4조 ')),
+    tags$head(tags$style('h1 {color:pink;font-size:50px;}')),
+    
     # 2. 
-    navlistPanel(
+    navlistPanel(widths = c(3,9),
         id = "tabset",
         "행복 지수",
         tabPanel('내가 정하는 행복 지수',
-                 sliderInput('weight_E',
-                             'Economy: ',
-                             min = 1,
-                             max = 5,
-                             value = 1,
-                             step = 0.01),
-                 sliderInput('weight_F',
-                             'Family: ',
-                             min = 1,
-                             max = 5,
-                             value = 1,
-                             step = 0.01),
-                 sliderInput('weight_H',
-                             'Healthy: ',
-                             min = 1,
-                             max = 5,
-                             value = 1,
-                             step = 0.01),
-                 sliderInput('weight_Fr',
-                             'Freedom: ',
-                             min = 1,
-                             max = 5,
-                             value = 1,
-                             step = 0.01),
-                 sliderInput('weight_T',
-                             'Trust: ',
-                             min = 1,
-                             max = 5,
-                             value = 1,
-                             step = 0.01),
-                 sliderInput('weight_G',
-                             'Generosity: ',
-                             min = 1,
-                             max = 5,
-                             value = 1,
-                             step = 0.01),
-                 sliderInput('weight_D',
-                             'Dystopia Residual: ',
-                             min = 1,
-                             max = 5,
-                             value = 1,
-                             step = 0.01),
-                 plotOutput('plots999', height = 1000, click = 'plot_click')),
+                 fluidRow(
+                     column(3,
+                            sliderInput('weight_E',
+                                        'Economy: ',
+                                        min = 1,
+                                        max = 5,
+                                        value = 1,
+                                        step = 0.01),
+                            sliderInput('weight_F',
+                                        'Family: ',
+                                        min = 1,
+                                        max = 5,
+                                        value = 1,
+                                        step = 0.01),
+                            sliderInput('weight_H',
+                                        'Healthy: ',
+                                        min = 1,
+                                        max = 5,
+                                        value = 1,
+                                        step = 0.01),
+                            sliderInput('weight_Fr',
+                                        'Freedom: ',
+                                        min = 1,
+                                        max = 5,
+                                        value = 1,
+                                        step = 0.01),
+                            sliderInput('weight_T',
+                                        'Trust: ',
+                                        min = 1,
+                                        max = 5,
+                                        value = 1,
+                                        step = 0.01),
+                            sliderInput('weight_G',
+                                        'Generosity: ',
+                                        min = 1,
+                                        max = 5,
+                                        value = 1,
+                                        step = 0.01),
+                            sliderInput('weight_D',
+                                        'Dystopia Residual: ',
+                                        min = 1,
+                                        max = 5,
+                                        value = 1,
+                                        step = 0.01)),
+                     column(9,
+                            plotOutput('plots999', height = 1000)),)
+                 ),
         tabPanel('대륙별 데이터',
                  selectInput('sel3',
                              'y column: ',
@@ -71,24 +78,28 @@ ui <- fluidPage(
                  plotOutput('plotkorea')),
         "여러 지수",
         tabPanel('행복에 대한 다양한 변수의 상관관계',
-                 selectInput('sel1',
-                             'x column: ',
-                             list('Economy (GDP per Capita)',
-                                  'Family',
-                                  'Health (Life Expectancy)',
-                                  'Freedom',
-                                  'Trust (Government Corruption)',
-                                  'Generosity',
-                                  'Dystopia Residual')),
-                 selectInput('sel2',
-                             'y column: ',
-                             list('Economy (GDP per Capita)',
-                                  'Family',
-                                  'Health (Life Expectancy)',
-                                  'Freedom',
-                                  'Trust (Government Corruption)',
-                                  'Generosity',
-                                  'Dystopia Residual')),
+                 fluidRow(
+                     column(6,
+                            selectInput('sel1',
+                                        'x column: ',
+                                        list('Economy (GDP per Capita)',
+                                             'Family',
+                                             'Health (Life Expectancy)',
+                                             'Freedom',
+                                             'Trust (Government Corruption)',
+                                             'Generosity',
+                                             'Dystopia Residual'))),
+                     column(6,
+                            selectInput('sel2',
+                                        'y column: ',
+                                        list('Economy (GDP per Capita)',
+                                             'Family',
+                                             'Health (Life Expectancy)',
+                                             'Freedom',
+                                             'Trust (Government Corruption)',
+                                             'Generosity',
+                                             'Dystopia Residual')))
+                 ),
                  plotOutput('plots')),
         "GIF 움직이는 사진",
         tabPanel('GDP .vs. 행복도지수',
@@ -181,8 +192,12 @@ df2015$Continent[which(is.na(df2015$Continent))] <- "Africa"
 server <- function(input,output){
     output$plots <- renderPlot({
         ggplot(df2015, aes(x=get(input$sel1), y=get(input$sel2))) + 
-            geom_point(size=4, color = 'coral1') +
-            theme_ipsum()
+            geom_point(size=4, color = 'coral1') + geom_smooth() +
+            labs(x = input$sel1, y = input$sel2) +
+            theme(axis.text.x = element_text(color = "grey20", size = 12, angle = 0),
+                  axis.text.y = element_text(color = "grey20", size = 12, angle = 0),  
+                  axis.title.x = element_text(color = "grey20", size = 20, angle = 0),
+                  axis.title.y = element_text(color = "grey20", size = 20, angle = 90))
     })
     
     output$plots999 <- renderPlot({
@@ -193,7 +208,7 @@ server <- function(input,output){
             df2015[,'Trust (Government Corruption)']*input$weight_T +
             df2015[,'Generosity']*input$weight_G +
             df2015[,'Dystopia Residual']*input$weight_D
-        ggplot(df2015, aes(x=Country, y=score), width = 600, height = 3000) +
+        ggplot(df2015, aes(x=Country, y=score), width = 3000, height = 3500) +
             geom_segment( aes(x=Country, xend=Country, y=0, yend=score), color="skyblue") +
             geom_point( color="blue", size=4, alpha=0.6) +
             theme_light() +
@@ -201,14 +216,18 @@ server <- function(input,output){
             theme(
                 panel.grid.major.y = element_blank(),
                 panel.border = element_blank(),
-                axis.ticks.y = element_blank(),
-                aspect.ratio=6/3
+                axis.ticks.y = element_blank()
             )
     })
     output$plotbox <- renderPlot({
         ggplot(df2015 , aes(x = Continent, y = get(input$sel3))) +
-            geom_boxplot(aes(fill=Continent)) + theme_bw() +
-            theme(axis.title = element_text(size = (8)))
+            geom_boxplot(aes(fill=Continent)) + theme_bw() + ylab(input$sel3) +
+            theme(axis.title = element_text(size = (8)),
+                  axis.text.x = element_text(color = "grey20", size = 12, angle = 0),
+                  axis.text.y = element_text(color = "grey20", size = 12, angle = 0),  
+                  axis.title.x = element_text(color = "grey20", size = 20, angle = 0),
+                  axis.title.y = element_text(color = "grey20", size = 20, angle = 90),
+                  legend.text = element_text(color = "grey20", size = 12, angle = 0))
     })
     output$plotkorea <- renderPlot({
         hp_score <- score %>% 
@@ -225,7 +244,12 @@ server <- function(input,output){
                  subtitle = "2006 ~ 2019",
                  caption = "World Happiness report") +
             theme_ipsum(grid = "Y") +
-            geom_hline(yintercept = 5.445809501, color = "#668b8b", size = 0.5)
+            geom_hline(yintercept = 5.445809501, color = "#668b8b", size = 0.5) +
+            theme(plot.title = element_text(size = 20),
+                  axis.text.x = element_text(color = "grey20", size = 12, angle = 0),
+                  axis.text.y = element_text(color = "grey20", size = 12, angle = 0),  
+                  axis.title.x = element_text(color = "grey20", size = 20, angle = 0),
+                  axis.title.y = element_text(color = "grey20", size = 20, angle = 90))
         
     })
     output$plotAni2 <- renderImage({
@@ -237,11 +261,15 @@ server <- function(input,output){
             ggplot( aes(x=year, y=-Happiness.Rank, group=Country, color=Country)) +
             geom_line() +
             geom_point() +
-            # scale_color_viridis(discrete = TRUE) +
             ggtitle("상위 10개 국가 연도별 행복지수 순위") +
-            theme_ipsum() +
             ylab("행복지수 순위") +
-            transition_reveal(year)
+            transition_reveal(year) +
+            theme(plot.title = element_text(size = 20),
+                  axis.text.x = element_text(color = "grey20", size = 12, angle = 0),
+                  axis.text.y = element_text(color = "grey20", size = 12, angle = 0),  
+                  axis.title.x = element_text(color = "grey20", size = 20, angle = 0),
+                  axis.title.y = element_text(color = "grey20", size = 20, angle = 90),
+                  legend.text = element_text(color = "grey20", size = 12, angle = 0))
         anim_save("outfile.gif", animate(p))
         list(src = "outfile.gif",
              contentType = 'image/gif')
@@ -254,7 +282,13 @@ server <- function(input,output){
                 theme_bw() +
                 labs(title = 'Year: {frame_time}', x = 'GDP', y = 'Happiness.score') +
                 transition_time(year) +
-                ease_aes('linear')
+                ease_aes('linear') +
+                theme(plot.title = element_text(size = 20),
+                    axis.text.x = element_text(color = "grey20", size = 12, angle = 0),
+                    axis.text.y = element_text(color = "grey20", size = 12, angle = 0),  
+                    axis.title.x = element_text(color = "grey20", size = 20, angle = 0),
+                    axis.title.y = element_text(color = "grey20", size = 20, angle = 90),
+                    legend.text = element_text(color = "grey20", size = 12, angle = 0))
         anim_save("outfile.gif", animate(p))
         list(src = "outfile.gif",
              contentType = 'image/gif'
